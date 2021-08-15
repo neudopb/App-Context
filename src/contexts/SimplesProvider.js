a721663f305e031916319c76ee5defe1a04e0500
neudinho11 <neudinho66@gmail.com>
2021-08-15 17:41:14 -0300

    Criação do SimplesProvider

--- src/contexts/SimplesProvider.js
+++ src/contexts/SimplesProvider.js
@@ -1,1 +1,24 @@
-import React, { createContext } from 'react';
+import React, { useState, createContext } from 'react';
+
+const SimplesContext = createContext({});
+
+export function SimplesProvider({ children }) {
+
+    const [nome, setNome] = useState('');
+    const [cpf, setCpf] = useState('');
+    const [email, setEmail] = useState('');
+    const [escolaridade, setEscolaridade] = useState('');
+    const [idioma, setIdioma] = useState('');
+    const [experiencia, setExperiencia] = useState('');
+    
+    return (
+        <SimplesContext.Provider value={{
+            nome, setNome, cpf, setCpf, email, setEmail, escolaridade,
+            setEscolaridade, idioma, setIdioma, experiencia, setExperiencia
+        }}>
+            { children }
+        </SimplesContext.Provider>
+    );
+};
+
+export default SimplesContext;
